ff4c9ec575e1d4a38396b608741f5c136edc12e9
Jax Bulbrook <jaxhacks543@gmail.com>
2024-10-13 15:25:49 -0400

    adding move sorting

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,10 @@
 use crate::structs::*;
 
+const PAWN_VALUE: i32 = 100;
+const KNIGHT_VALUE: i32 = 300;
+const BISHOP_VALUE: i32 = 300;
+const ROOK_VALUE: i32 = 500;
+const QUEEN_VALUE: i32 = 900;
 
 pub fn search(board: &mut Board, depth: usize, mut alpha: i32, beta: i32) -> (i32, Option<Move>) {
     if depth == 0 {
@@ -13,6 +18,7 @@ pub fn search(board: &mut Board, depth: usize, mut alpha: i32, beta: i32) -> (i3
         }
         return (0, None);
     }
+    let moves = order_moves(board, moves);
     for r#move in moves {
         let castling_rights = board.castling_rights.clone();
         let enpassant_square = board.enpassant_square;
@@ -43,24 +49,41 @@ fn eval(board: &Board) -> i32 {
     evaluation * perspective
 }
 fn count_material(board: &Board, color: Color) -> i32 {
-    const PAWN_VALUE: i32 = 100;
-    const KNIGHT_VALUE: i32 = 300;
-    const BISHOP_VALUE: i32 = 300;
-    const ROOK_VALUE: i32 = 500;
-    const QUEEN_VALUE: i32 = 900;
     let mut material = 0;
     for (_, piece) in &board.pieces {
         if piece.color != color {
             continue;
         }
-        match piece.r#type {
-            Pawn => material += PAWN_VALUE,
-            Knight => material += KNIGHT_VALUE,
-            Bishop => material += BISHOP_VALUE,
-            Rook => material += ROOK_VALUE,
-            Queen => material += QUEEN_VALUE,
-            King => (),
-        }
+        material += get_piece_value(&piece.r#type);
     }
     material
+}
+fn order_moves(board: &Board, mut moves: Vec<Move>) -> Vec<Move> {
+    for r#move in moves {
+        let mut score_guess = 0;
+        let move_piece_type = board.pieces.get(&r#move.from);
+        let capture_piece_type = board.pieces.get(&r#move.to);
+        // prioritise capturing opponent's most valuable pieces with our least valuable pieces
+        if let None = capture_piece_type {
+            score_guess = 10 * get_piece_value(&capture_piece_type.unwrap().r#type) - get_piece_value(&move_piece_type.unwrap().r#type);
+        }
+        //promoting a pawn is probably good
+        if r#move.promotion.is_some() {
+            score_guess += get_piece_value(&r#move.promotion.unwrap());
+        }
+    }
+    sort_moves(moves)
+}
+fn sort_moves(mut moves: Vec<Move>) -> Vec<Move> {
+
+}
+fn get_piece_value(piece: &PieceType) -> i32 {
+    match piece {
+        Pawn => PAWN_VALUE,
+        Knight => KNIGHT_VALUE,
+        Bishop => BISHOP_VALUE,
+        Rook => ROOK_VALUE,
+        Queen => QUEEN_VALUE,
+        King => 0,
+    }
 }
